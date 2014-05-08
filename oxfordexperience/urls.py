5565570307d730fef19eefdaf7dbb47de66d1e69
Sara Palmer <sepalme@emory.edu>
2014-05-08 15:33:23 -0400

    updated urls

--- oxfordexperience/urls.py
+++ oxfordexperience/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.views.generic.simple import redirect_to
+#from django.views.generic.simple import redirect_to
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
