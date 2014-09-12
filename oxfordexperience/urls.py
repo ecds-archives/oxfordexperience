20f2f4fcfda1766cae7378d7afa08c9aa5393ff2
Sara Palmer <sepalme@emory.edu>
2014-09-12 16:18:53 -0400

    update

--- oxfordexperience/urls.py
+++ oxfordexperience/urls.py
@@ -1,9 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.conf.urls.defaults import *
 from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-#from django.views.generic.simple import redirect_to
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -23,13 +19,4 @@ urlpatterns = patterns('oxex.views',
 if settings.DEBUG:
   urlpatterns += patterns(
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT } ),
-)
-
-
-    #Uncomment the admin/doc line below to enable admin documentation:
-    #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    #url(r'^admin/', include(admin.site.urls)),
-    #)
-    
+)  
